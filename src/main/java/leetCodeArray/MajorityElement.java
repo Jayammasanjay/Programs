101dcbc1e97e6c8ad955a32b7b618de5a9fd5acb
Sanjay N <sanjayyaksha3009@gmail.com>
2024-11-06 11:55:14 +0530

    mylogic in MajorityElement leetcode

--- src/main/java/leetCodeArray/MajorityElement.java
+++ src/main/java/leetCodeArray/MajorityElement.java
@@ -1,5 +1,7 @@
 package leetCodeArray;
 
+import java.util.Arrays;
+
 public class MajorityElement {
 //time o(n) space O(1)
     public static void main(String[] args) {
@@ -7,20 +9,40 @@ public class MajorityElement {
         System.out.println(findMajority(arr));
     }
 
+//    public static int findMajority(int[] arr){
+//        int candidate=0;
+//        int count=0;
+//        for(int i=0;i<arr.length;i++){
+//            if(count==0){
+//                candidate=arr[i];
+//            }
+//            if(candidate==arr[i]){
+//                count++;
+//            }else {
+//                count--;
+//            }
+//        }
+//
+//        return candidate;
+//    }
+
     public static int findMajority(int[] arr){
-        int candidate=0;
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            if(count==0){
-                candidate=arr[i];
-            }
-            if(candidate==arr[i]){
+        int count=1;
+        int maxtime=0;
+        int currcount=0;
+        Arrays.sort(arr);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]){
                 count++;
-            }else {
-                count--;
+            }
+            else{
+                break;
+            }
+            if(count>currcount){
+                currcount=count;
+                maxtime=arr[i];
             }
         }
-
-        return candidate;
+        return maxtime;
     }
 }
