fe2650f12c8984fcfc8bf52fe7252c2f42cc5049
Sanjay N <sanjayyaksha3009@gmail.com>
2024-11-07 16:26:14 +0530

    SearchinRotatedSortedArrayII leetcode

---
+++ src/main/java/leetCodeArray/SearchinRotatedSortedArrayII.java
@@ -0,0 +1,38 @@
+package leetCodeArray;
+
+public class SearchinRotatedSortedArrayII {
+    public static void main(String[] args) {
+
+        int[] nums = {2,5,6,0,0,1,2}; int target = 0;
+        boolean res=find(nums,target);
+        System.out.println(res);
+    }
+    public static boolean find(int[] arr,int target){
+        int left=0,right=arr.length-1;
+        while(left<=right){
+           int mid=(left+right)/2;
+           if(arr[mid]==target){
+               return true;
+           }
+           if(arr[left]==arr[mid] && arr[right]==arr[mid]){
+               left++;
+               right--;
+           }
+           else if (arr[left]<=arr[mid]){
+               if(arr[left]<=target && target<arr[mid]){
+                   right=mid-1;
+               }else {
+                   left=mid+1;
+               }
+           }
+           else{
+               if(arr[mid]<target && target<=arr[right]){
+                   left=mid+1;
+               }else {
+                   right=mid-1;
+               }
+           }
+        }
+        return false;
+    }
+}
